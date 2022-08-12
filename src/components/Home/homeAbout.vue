f47146e9898f7c5d295eee404f4bc1d878edf1aa
coderdal <agircoder@gmail.com>
2022-08-12 22:53:45 +0300

    added functionality to about section & media queries

--- src/components/Home/homeAbout.vue
+++ src/components/Home/homeAbout.vue
@@ -22,6 +22,29 @@
             Join over 700,000 users from all over the world satisfied with our
             services.
           </p>
+          <div class="text-buttons">
+            <p
+              @click="activeText = textArray[0]"
+              :class="{ active: activeText === textArray[0] }"
+            >
+              Our Mission
+            </p>
+            <span class="divide-line"></span>
+            <p
+              @click="activeText = textArray[1]"
+              :class="{ active: activeText === textArray[1] }"
+            >
+              Our Advantages
+            </p>
+            <span class="divide-line"></span>
+            <p
+              @click="activeText = textArray[2]"
+              :class="{ active: activeText === textArray[2] }"
+            >
+              Our guarantees
+            </p>
+          </div>
+          <p class="active-text">{{ activeText }}</p>
         </div>
       </div>
     </div>
@@ -31,6 +54,17 @@
 <script>
 export default {
   name: "homeAbout",
+  data() {
+    return {
+      textArray: [
+        "Bitcoin is based on a protocol known as the blockchain, which allows to create, transfer and verify ultra-secure financial data without interference of third parties.",
+        "Our mission as an official partner of Bitcoin Foundation is to help you enter and better understand the world of #1 cryptocurrency and avoid any issues you may encounter.",
+        "We are here because we are passionate about open, transparent markets and aim to be a major driving force in widespread adoption, we are the first and the best in cryptocurrency.",
+      ],
+      activeText:
+        "Bitcoin is based on a protocol known as the blockchain, which allows to create, transfer and verify ultra-secure financial data without interference of third parties.",
+    };
+  },
 };
 </script>
 
@@ -100,6 +134,7 @@ export default {
 .about .content .about-us .text-content {
   text-align: left;
   padding-right: 10px;
+  padding-left: 30px;
 }
 
 .about .content .about-us .text-content h2 {
@@ -108,8 +143,54 @@ export default {
   font-size: 1.6em;
 }
 
-.about .content .about-us .text-content p {
+.about .content .about-us .text-content > p {
   color: var(--text-lighter-color);
   margin-top: 10px;
+}
+
+.about .content .about-us .text-content .text-buttons {
+  width: 100%;
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  margin-top: 22px;
+}
+
+.about .content .about-us .text-content .text-buttons p {
+  text-transform: uppercase;
+  color: var(--text-lighter-color);
+  cursor: pointer;
+}
+
+.about .content .about-us .text-content .text-buttons p.active {
+  color: var(--orange-color);
+  font-weight: 600;
+}
+
+.about .content .about-us .text-content .text-buttons .divide-line {
+  width: 1px;
+  height: 26px;
+  background: var(--text-lighter-color);
+}
+
+.about .content .about-us .text-content .active-text {
+  margin-top: 16px;
+}
+
+/* Media Queries */
+
+@media screen and (max-width: 1140px) {
+  .about .content .about-us .text-content .text-buttons .divide-line {
+    height: 44px;
+  }
+}
+
+@media screen and (max-width: 915px) {
+  .about .content .about-us {
+    flex-direction: column;
+  }
+  .about .content .about-us .text-content .text-buttons .divide-line {
+    height: 26px;
+  }
 }
 </style>
