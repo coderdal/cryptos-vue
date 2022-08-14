a1e48df487fad691e4f3b10e5d29093778e92e98
coderdal <agircoder@gmail.com>
2022-08-14 21:33:38 +0300

    home features created

---
+++ src/components/Home/homeFeatures.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div class="content">
+      <div class="boxes">
+        <h2>example feature box</h2>
+        <h2>example feature box</h2>
+        <h2>example feature box</h2>
+      </div>
+
+      <div class="video-side">
+        <h4>video player</h4>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "homeFeatures",
+};
+</script>
+
+<style scoped>
+</style>
